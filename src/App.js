b96ec88da834512537b30274f2295d59197172ec
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-07-05 15:11:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Mars Rover Photo Viewer
+      <div className="form">
+        <input type="number" name="sol" id="sol" min="1" />
+        <select name="camera" id="camera">
+          <option value="blah">blah</option>
+          <option value="bleh">bleh</option>
+        </select>
+        <button type="submit">Submit</button>
+      </div>
     </div>
   );
 }
