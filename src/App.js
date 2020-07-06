6f4c14995429cd6ee9ffc5127256a4c687576736
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-07-06 09:02:37 +0530

    Add camera parameters

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+  const NasaAPIKey = process.env.REACT_APP_API_KEY;
+  //console.log(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${NasaAPIKey}`);
   return (
     <div className="App">
-      Mars Rover Photo Viewer
+      Mars Curiosity Rover Photo Viewer
       <div className="form">
-        <input type="number" name="sol" id="sol" min="1" />
+        <input type="number" name="sol" id="sol" min="0" />
         <select name="camera" id="camera">
-          <option value="blah">blah</option>
-          <option value="bleh">bleh</option>
+          <option value="FHAZ">Front Hazard Avoidance Camera</option>
+          <option value="RHAZ">Rear Hazard Avoidance Camera</option>
+          <option value="MAST">Mast Camera</option>
+          <option value="CHEMCAM">Chemistry and Camera Complex</option>
+          <option value="MAHLI">Mars Hand Lens Imager</option>
+          <option value="MARDI">Mars Descent Imager</option>
+          <option value="NAVCAM">Navigation Camera</option>
         </select>
         <button type="submit">Submit</button>
       </div>
