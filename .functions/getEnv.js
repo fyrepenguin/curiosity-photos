7bc33e01ae5d1c68a0b9acd927b2e60d4f3a49e2
Mourya Prasanth <mouryaprasanthp@gmail.com>
2020-07-07 19:27:40 +0530

    Add netlify.toml file

--- .functions/getEnv.js
+++ .functions/getEnv.js
@@ -1,8 +1,7 @@
 
 
 exports.handler = async (event, context) => {
-  let env = process.env
-  console.log(env)
+  let env = process.env.REACT_APP_API_KEY
   return {
     statusCode: 200,
     body: JSON.stringify(env)
